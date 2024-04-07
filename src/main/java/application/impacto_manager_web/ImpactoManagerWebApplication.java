b527f1effd6ae551cd396131b146ae4c79406654
Rhama Krisner <rhamakrisner@gmail.com>
2024-04-06 22:24:13 -0300

    Inicado o projeto

---
+++ src/main/java/application/impacto_manager_web/ImpactoManagerWebApplication.java
@@ -0,0 +1,13 @@
+package application.impacto_manager_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactoManagerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpactoManagerWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/application/impacto_manager_web/ImpactoManagerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package application.impacto_manager_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactoManagerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
