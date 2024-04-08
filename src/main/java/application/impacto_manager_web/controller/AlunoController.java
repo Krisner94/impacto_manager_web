b9290fea15f5708e0daa0b204b08daaa76cc736c
Rhama Krisner <rhamakrisner@gmail.com>
2024-04-08 00:07:06 -0300

    Adicionado mais configuraçoes de documentação

--- src/main/java/application/impacto_manager_web/controller/AlunoController.java
+++ src/main/java/application/impacto_manager_web/controller/AlunoController.java
@@ -2,6 +2,7 @@ package application.impacto_manager_web.controller;
 
 import application.impacto_manager_web.model.Aluno;
 import application.impacto_manager_web.service.AlunoService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,29 +17,34 @@ public class AlunoController {
         this.service = service;
     }
 
-    @GetMapping({"","/"})
-    public List<Aluno> findAll(){
+    @GetMapping({"", "/"})
+    @Operation(summary = "Buscar tudo", description = "Busca todos os alunos", tags = "Alunos")
+    public List<Aluno> findAll() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar por id", description = "Busca aluno por id", tags = "Alunos")
     public Aluno findById(@PathVariable Long id) {
         return service.findById(id);
     }
 
     @PostMapping
-    public ResponseEntity<Aluno> create(@RequestBody Aluno aluno){
+    @Operation(summary = "Novo Aluno", description = "Criar novo aluno", tags = "Alunos")
+    public ResponseEntity<Aluno> create(@RequestBody Aluno aluno) {
         Aluno novoAluno = service.save(aluno);
         return ResponseEntity.ok(novoAluno);
     }
 
     @PutMapping("/{id}")
-    public Aluno update(@PathVariable Long id, @RequestBody Aluno aluno){
+    @Operation(summary = "Editar dados", description = "Editar dados de alunos", tags = "Alunos")
+    public Aluno update(@PathVariable Long id, @RequestBody Aluno aluno) {
         return service.update(id, aluno);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    @Operation(summary = "Exclui aluno", description = "Exclui dados de alunos", tags = "Alunos")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.ok().build();
     }
