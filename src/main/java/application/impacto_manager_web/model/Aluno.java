6d2096b3a0853550bd1bab6605fd80763e800f50
Rhama Krisner <rhama.krisner@gmail.com>
2024-08-18 00:58:36 -0300

    refactored: Added IDENTITY type for model

--- src/main/java/application/impacto_manager_web/model/Aluno.java
+++ src/main/java/application/impacto_manager_web/model/Aluno.java
@@ -22,7 +22,7 @@ public class Aluno implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String nome;
