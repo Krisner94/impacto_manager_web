416488ba56efc417dce27b30b134879f9e3b025b
Rhama Krisner <rhama.krisner@gmail.com>
2024-08-18 00:09:31 -0300

    refactored: Deletado imports não usados

--- src/main/java/application/impacto_manager_web/ImpactoManagerWebApplication.java
+++ src/main/java/application/impacto_manager_web/ImpactoManagerWebApplication.java
@@ -1,11 +1,5 @@
 package application.impacto_manager_web;
 
-import application.impacto_manager_web.enums.DiaDaSemanaEnum;
-import application.impacto_manager_web.model.Professor;
-import application.impacto_manager_web.model.Turma;
-import application.impacto_manager_web.repository.ProfessorRepository;
-import application.impacto_manager_web.repository.TurmaRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
