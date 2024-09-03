de62317c8fe634becb78fd12fee3bf5f0343776f
Rhama Krisner <rhama.krisner@gmail.com>
2024-09-03 18:33:11 -0300

    refactor: Adicionado Bean ao GetDBConnection.java

--- src/main/java/application/impacto_manager_web/utils/GetDBConnection.java
+++ src/main/java/application/impacto_manager_web/utils/GetDBConnection.java
@@ -3,7 +3,9 @@ package application.impacto_manager_web.utils;
 import application.impacto_manager_web.repository.AlunoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class GetDBConnection {
     private final AlunoRepository respository;
