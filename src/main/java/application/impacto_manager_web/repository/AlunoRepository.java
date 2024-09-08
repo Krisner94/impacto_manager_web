d2fce221e131c85fc2251006544a6e9d759855b5
Rhama Krisner <rhama.krisner@gmail.com>
2024-09-08 00:20:33 -0300

    feat: Adicionado filtragem por nome e cpf

--- src/main/java/application/impacto_manager_web/repository/AlunoRepository.java
+++ src/main/java/application/impacto_manager_web/repository/AlunoRepository.java
@@ -2,8 +2,15 @@ package application.impacto_manager_web.repository;
 
 import application.impacto_manager_web.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+    @Query("SELECT a FROM Aluno a WHERE (:nome IS NULL OR LOWER(a.nome) LIKE %:nome%) AND (:cpf IS NULL OR a.cpf LIKE %:cpf%)")
+    List<Aluno> findByNomeOrCpf(@Param("nome") String nome, @Param("cpf") String cpf);
 }
