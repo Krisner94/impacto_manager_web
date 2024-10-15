502f9af4c73cafdc7d08a153da36d855c818415c
Rhama Krisner <rhama.krisner@gmail.com>
2024-10-14 22:24:30 -0300

    refactor: Adicionando novo Scheduler

--- src/main/java/application/impacto_manager_web/utils/PingScheduler.java
+++ src/main/java/application/impacto_manager_web/utils/PingScheduler.java
@@ -1,30 +1,28 @@
 package application.impacto_manager_web.utils;
 
-import application.impacto_manager_web.model.Turma;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import application.impacto_manager_web.service.AlunoService;
 import application.impacto_manager_web.service.TurmaService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 @EnableScheduling
 @RequiredArgsConstructor
 public class PingScheduler {
+    private final TurmaService service;
 
-    private final TurmaService turmaService;
-
-    @Scheduled(fixedRate = 180000) // A cada 180.000 milissegundos (3 minutos)
+    // Cron expression to schedule every 15 minutes (adjust as needed)
+    @Scheduled(cron = "*/5 * * * * *")
     public void pingHost() {
-        List<Turma> turmas = turmaService.findAll();
-        log.info("Executed getAll on TurmaService. Fetched {} turmas.", turmas.size());
-
+        log.info("{}",service.findAll());
     }
 }
