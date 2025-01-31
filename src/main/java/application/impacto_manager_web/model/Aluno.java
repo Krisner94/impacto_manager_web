6c1a86d7ef30e8c1cd796cea20775eb316c2654e
Krisner94 <rhama.krisner@gmail.com>
2025-01-30 23:40:07 -0300

    refactor: Mudança de tipo de numeração de ID no banco de dados

--- src/main/java/application/impacto_manager_web/model/Aluno.java
+++ src/main/java/application/impacto_manager_web/model/Aluno.java
@@ -24,7 +24,7 @@ public class Aluno implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column(nullable = false)
     private String nome;
